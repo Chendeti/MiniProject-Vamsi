d9b501b8caadfdb7fccf44127111d9db27fefbcc
Chendeti <104186390+Chendeti@users.noreply.github.com>
2022-06-15 17:18:58 +0530

    Add files via upload

---
+++ 33-MiniProject/src/main/java/com/zensar/rest/ComplianceController.java
@@ -0,0 +1,35 @@
+package com.zensar.rest;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zensar.bean.Compliance;
+import com.zensar.bean.StatusReport;
+import com.zensar.service.ComplianceService;
+
+@RestController
+public class ComplianceController {
+
+	@Autowired
+	ComplianceService service;
+
+	@PostMapping(value = "/createRL")
+	public Compliance f1(@RequestBody Compliance co) {
+		return this.service.createRL(co);
+	}
+
+	@GetMapping(value = "/compliances")
+	public List<Compliance> f2() {
+		return this.service.getAllCompliances();
+	}
+
+	@PostMapping(value = "/statusreport")
+	public void f3(@RequestBody StatusReport statusreport) {
+		this.service.createStatusReport(statusreport);
+	}
+}
